0d4a1b0f33d10c2ed0911b6da3ad242249626e1d
DESKTOP- caleb <caleb.wigwubgames@gmail.com>
2026-05-01 16:54:54 -0400

    feat: add Die class

---
+++ seal-the-box/scripts/match/die.gd
@@ -0,0 +1,14 @@
+class_name Die
+extends RefCounted
+
+var faces: int
+var value: int = 0
+var rolled: bool = false
+
+func _init(f: int) -> void:
+    faces = f
+
+func roll() -> int:
+    value = randi_range(1, faces)
+    rolled = true
+    return value
