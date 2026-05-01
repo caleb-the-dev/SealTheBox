4711b9a48ac60db612b035f2a83beaafe90870e1
DESKTOP- caleb <caleb.wigwubgames@gmail.com>
2026-05-01 16:55:11 -0400

    feat: add AbilityData resource and abilities.csv

---
+++ seal-the-box/resources/ability_data.gd
@@ -0,0 +1,10 @@
+class_name AbilityData
+extends Resource
+
+@export var id: String = ""
+@export var flavor_name: String = ""
+@export var type: String = ""
+@export var traits: Array[String] = []
+@export var cooldown: int = 0
+@export var ap_cost: int = 1
+@export var description: String = ""
